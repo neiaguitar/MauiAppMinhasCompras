92d56f987fc7b2457880dd105b1c59ec826156f4
neiaguitar <neiaguitar@gmail.com>
2025-03-02 14:27:09 -0300

    Alterações feitas na agenda 03

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -5,5 +8,25 @@ public partial class NovoProduto : ContentPage
 	public NovoProduto()
 	{
 		InitializeComponent();
+	}
+
+	private async void ToolbarItem_Clicked(object sender, EventArgs e) 
+	{
+		try 
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registro Inserido", "Ok");
+
+		} catch(Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "Ok");
+		}
 	}
 }
